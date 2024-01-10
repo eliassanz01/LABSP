8ea88a325fa4c1dfb9a83e83637a0d9ed8ffda68
Ignasipm00 <49555846+Ignasipm00@users.noreply.github.com>
2024-01-10 15:06:39 +0100

    Update normalize.py

--- normalize.py
+++ normalize.py
@@ -1,8 +1,7 @@
 def normalize(df,op):
+    numeric_vals = df._get_numeric_data()
     if op == 0:
-        numeric_vals = df._get_numeric_data()
         norm_df=(numeric_vals-numeric_vals.mean())/numeric_vals.std()
     else:
-        numeric_vals = df._get_numeric_data()
         norm_df=(numeric_vals - numeric_vals.min()) / (numeric_vals.max() - numeric_vals.min())
     return norm_df
