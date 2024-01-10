7a10375181db986254c31405fcb96821cc63c75f
Ignasipm00 <49555846+Ignasipm00@users.noreply.github.com>
2024-01-10 18:47:12 +0100

    Update dataframe_editor.py

--- dataframe_editor.py
+++ dataframe_editor.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
-def dataframe_editor(dataframe):
-    aux = dataframe.name.str.split('_',expand=True)
-    aux.columns = ['phon','R','subject_id','trial']
-    dataframe['subject_id'] = aux.subject_id
-    dataframe['trial'] = aux.trial
-    dataframe.drop(dataframe.columns[[0]], axis=1, inplace=True)
-    return dataframe
+aux = df.name.str.split('_',expand=True)
+aux.columns = ['phon','R','subject_id','trial']
+df['subject_id'] = aux.subject_id
+df['trial'] = aux.trial
+df.drop(df.columns[[0]], axis=1, inplace=True)
+new_df=df
