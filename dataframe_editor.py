d52ba94ee96f2601d6c2f3affbf9d4e81d9fd0fd
Ignasipm00 <49555846+Ignasipm00@users.noreply.github.com>
2024-01-10 16:33:03 +0100

    Update dataframe_editor.py

--- dataframe_editor.py
+++ dataframe_editor.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-df=pd.read_csv(dataframe)
-aux = df.name.str.split('_',expand=True)
-aux.columns = ['phon','R','subject_id','trial']
-df['subject_id'] = aux.subject_id
-df['trial'] = aux.trial
-df_new=df.drop(['name'], axis=1)
+def dataframe_editor(dataframe):
+    aux = dataframe.name.str.split('_',expand=True)
+    aux.columns = ['phon','R','subject_id','trial']
+    dataframe['subject_id'] = aux.subject_id
+    dataframe['trial'] = aux.trial
+    dataframe.drop(dataframe.columns[[0]],axis=1,inplace=True)
+    return dataframe
