93999016d46a167f650feed50e33e050ed10d0ec
Ignasipm00 <49555846+Ignasipm00@users.noreply.github.com>
2024-01-10 15:17:54 +0100

    Update scat_plt.py

--- scat_plt.py
+++ scat_plt.py
@@ -5,7 +5,7 @@ import seaborn as sns
 #The obtained plot should contain a legend displaying the information regarding to groups
 
 def scat_plt(var1, var2, groups):
-    sns.scatterplot(x=var1, y=var2, c=groups)
+    sns.scatterplot(x=var1, y=var2, hue=groups)
     plt.show()
 
 
