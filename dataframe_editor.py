4b1e313bd28f5e0a8a7ec0a86268e400d17f8d34
Ignasipm00 <ignasi.prats@alumn.esci.upf.edu>
2024-01-10 15:46:11 +0000

    update

--- dataframe_editor.py
+++ dataframe_editor.py
@@ -1,11 +1,10 @@
 import pandas as pd
 
 def dataframe_editor(dataframe):
+    aux = dataframe.name.str.split('_',expand=True)
+    aux.columns = ['phon','R','subject_id','trial']
+    dataframe['subject_id'] = aux.subject_id
+    dataframe['trial'] = aux.trial
+    dataframe.drop(dataframe.columns[[0]], axis=1, inplace=True)
+    print(dataframe.head())
     return dataframe
-#    aux = dataframe.name.str.split('_',expand=True)
-#    aux.columns = ['phon','R','subject_id','trial']
-#    dataframe['subject_id'] = aux.subject_id
-#    dataframe['trial'] = aux.trial
-#    dataframe.drop(dataframe.columns[[0]], axis=1, inplace=True)
-#    print(dataframe.head())
-#    return dataframe
