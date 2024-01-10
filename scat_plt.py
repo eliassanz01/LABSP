33d94cc4478a53f88cd3a44e014a28353c4e6452
Ignasipm00 <49555846+Ignasipm00@users.noreply.github.com>
2024-01-10 10:03:52 +0100

    Update scat_plt.py

--- scat_plt.py
+++ scat_plt.py
@@ -1,1 +1,10 @@
+import seaborn as sns
+
+# Falta lo siguiente, lo actualizare en breves
+#The obtained plot should contain a legend displaying the information regarding to groups
+
+def scat_plt(var1, var2, groups):
+    sns.lineplot(x=var1, y=var2, c=groups)
+    plt.show()
+
 
